e00c9c9f19c106b87548b17ec1cfb97662093686
Adeogun Michael <32965427+mikkylulz@users.noreply.github.com>
2022-09-16 11:04:31 -0700

    Update main.js
    just testing

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ window.onload = screen;
 
 function screen () {
     myWidth = window.innerWidth;
-    document.getElementById('size').innerHTML = "width :" + myWidth + "px";
+     document.getElementById('size').innerHTML = "width :" + myWidth + "px";
 }
